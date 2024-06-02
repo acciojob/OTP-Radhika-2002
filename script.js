6e27259accf18e9ce6026f62f0eb41670fc56185
Radhika-2002 <2019radhika.katiyara@ves.ac.in>
2024-06-03 01:46:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes.forEach((code, idx) => {
+    code.addEventListener('input', (e) => {
+        const input = e.target;
+        const value = input.value;
+        if (value) {
+            if (idx < codes.length - 1) {
+                codes[idx + 1].focus();
+            }
+        }
+    });
+
+    code.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace') {
+            if (code.value === '') {
+                if (idx > 0) {
+                    codes[idx - 1].focus();
+                }
+            }
+        }
+    });
+});
